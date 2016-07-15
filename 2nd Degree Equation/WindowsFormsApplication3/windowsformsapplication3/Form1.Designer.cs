2badf6dba09497f6a9b6a5c04c4fe5919a20fd23
Emanuel Covaci <covaciemanuel98@gmail.com>
2016-07-15 21:04:26 +0300

    2nd degree equation-works

---
+++ 2nd Degree Equation/WindowsFormsApplication3/windowsformsapplication3/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(153, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(32, 25);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "a=";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(153, 70);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(32, 25);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "b=";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(153, 101);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(32, 25);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "c=";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(191, 39);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(68, 25);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(191, 70);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(68, 25);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(191, 101);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(68, 25);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(34, 132);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(382, 27);
+            this.textBox4.TabIndex = 6;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(34, 165);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(225, 90);
+            this.textBox5.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(419, 255);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.HelpButton = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Calculator ecuatia de gradul II";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+    }
+}
+
---
+++ 2nd Degree Equation/WindowsFormsApplication3/windowsformsapplication3/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void calcul()
+        {
+
+            long a = Convert.ToInt64(textBox1.Text);//citesc din casuta a un string si il transform in long
+            long b = Convert.ToInt64(textBox2.Text);//citesc din casuta b un string si il transform in long
+            long c = Convert.ToInt64(textBox3.Text);//citesc din casuta c un string si il transform in long
+
+            textBox4.Text = "Ecuatia e : ";//aici e algoritmul de afisare a ecuatiei
+            if (a > 0 || a < 0)
+            { textBox4.Text = textBox4.Text + a + "x*x"; }
+            if (b > 0)
+            {
+                if (a != 0)
+                { textBox4.Text = textBox4.Text + "+" + b + "x"; }
+                else { textBox4.Text = textBox4.Text + b + "x"; }
+            }
+            else
+            {
+                if (b < 0)
+                { textBox4.Text = textBox4.Text + " " + b + "x"; }
+            }
+            if (c > 0)
+            {
+                textBox4.Text = textBox4.Text + "+" + c;
+            }
+            else
+            {
+                if (c < 0)
+                { textBox4.Text = textBox4.Text + " " + c; }
+            }
+            textBox4.Text += " = 0 ";
+
+            double delta;
+            delta = (b * b) - (4 * a * c);//calculez delta
+            textBox5.Text = "Delta=" + delta + Environment.NewLine;
+            if (delta < 0)
+            {
+                textBox5.Text = textBox5.Text+"Ecuatia nu are solutii reale";
+                calculComplex(a, b, c, delta);//apelez o alta metoda(functie) care sa-mi calculeze pentru solutiile ireale
+
+            }
+            else
+            {
+                if (delta == 0)
+                {
+                    // double x = -(b / (2 * a));
+                   // Double  x = ((-b) / (2 * a));
+                    Double x = (-b - Math.Sqrt(delta)) / (2 * a);
+                    textBox5.Text =textBox5.Text+ "Solutia ecuatiei e =" + x;//afisez solutia ecuatiei
+                }
+                else
+                {
+
+                    Double x = (-b - Math.Sqrt(delta)) / (2 * a);//calculez radaciniile reale
+                    Double y = (-b + Math.Sqrt(delta)) / (2 * a);
+                    textBox5.Text = textBox5.Text + "Solutiile ecuatiei sunt:" + Environment.NewLine;//afisez radaciniile
+                    textBox5.Text = textBox5.Text + "x1=" + x + Environment.NewLine;
+                    textBox5.Text = textBox5.Text + "x2=" + y;
+
+                }
+            }
+        }
+        public void calculComplex(long a, long b, long c, double delta)//metoda care calculeaza radaciniile ireale
+        {
+            double rDelta = Math.Sqrt(-delta);
+            textBox5.Text += Environment.NewLine;
+            textBox5.Text += "Solutiile complexe:" + Environment.NewLine;
+            if (b < 0)
+            {
+                textBox5.Text = textBox5.Text +"x1="+ -b + " + " + rDelta + " i " + "/" + 2 * a + Environment.NewLine;
+                textBox5.Text = textBox5.Text + "x2="+ -b + " - " + rDelta + " i " + "/" + 2 * a + Environment.NewLine;
+            }
+            else
+            {
+                textBox5.Text = textBox5.Text +"x1="+ "-" + b + " + " + rDelta + " i " + "/" + 2 * a + Environment.NewLine;
+                textBox5.Text = textBox5.Text +"x2="+ "-" + b + " - " + rDelta + " i " + "/" + 2 * a + Environment.NewLine;
+            }
+
+
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            try//cu ajutorul lui try-catch ,ofer programului dinamica,de fiecare daca cand schimb o valoare a,b,c se recalculeaza instant iar daca o casuta e goala
+               //programul nu se blocheaza 
+               //cand am scris primadata codul foloseam un buton ok si reset,introduceam valoriile pentru a.b.c ,dadeam ok se calcula ,iar daca doream sa schimb valoriile 
+               //trebuia sa dau reset cu ajutorul try-catch problema sa rezolvat foarte usor si asa programul e si mai optim si mai dinamic 
+            {
+                calcul();
+            }
+            catch
+            {
+                textBox4.Text = "";//aici sterg informatiile din casutele de afisare
+                textBox5.Text = "";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
